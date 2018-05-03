3c456e89d5f81cad06da89afcdd2fe1e2fb53687
Héctor Lopez <hectorjl01@hectors-mbp.home>
2018-05-02 21:45:22 -0400

    rev6

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,76 +2,76 @@
 
 // Routes:-----------------------------------
 // Default
-app.get("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "friends.js"));
-  });
+// app.get("/api/friends", function(req, res) {
+//     res.sendFile(path.join(__dirname, "friends.js"));
+//   });
 
-  app.post("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "friends.js"));
-  });
-
-
+//   app.post("/api/friends", function(req, res) {
+//     res.sendFile(path.join(__dirname, "friends.js"));
+//   });
 
 
 
 
 
-// // Pull in required dependencies
-// var path = require('path');
 
-// // Import the list of friend entries
-// var friends = require('../data/friends.js');
 
-// // Export API routes
-// module.exports = function(app) {
-// 	// console.log('___ENTER apiRoutes.js___');
+// Pull in required dependencies
+var path = require('path');
 
-// 	// Total list of friend entries
-// 	app.get('/api/friends', function(req, res) {
-// 		res.json(friends);
-// 	});
+// Import the list of friend entries
+var friends = require('../data/friends.js');
 
-// 	// Add new friend entry
-// 	app.post('/api/friends', function(req, res) {
-// 		// Capture the user input object
-// 		var userInput = req.body;
-// 		// console.log('userInput = ' + JSON.stringify(userInput));
+// Export API routes
+module.exports = function(app) {
+	console.log('___ENTER apiRoutes.js___');
 
-// 		var userResponses = userInput.scores;
-// 		// console.log('userResponses = ' + userResponses);
+	// Total list of friend entries
+	app.get('/api/friends', function(req, res) {
+		res.json(friends);
+	});
 
-// 		// Compute best friend match
-// 		var matchName = '';
-// 		var matchImage = '';
-// 		var totalDifference = 10000; // Make the initial value big for comparison
+	// Add new friend entry
+	app.post('/api/friends', function(req, res) {
+		// Capture the user input object
+		var userInput = req.body;
+		console.log('userInput = ' + JSON.stringify(userInput));
 
-// 		// Examine all existing friends in the list
-// 		for (var i = 0; i < friends.length; i++) {
-// 			// console.log('friend = ' + JSON.stringify(friends[i]));
+		var userResponses = userInput.scores;
+		console.log('userResponses = ' + userResponses);
 
-// 			// Compute differenes for each question
-// 			var diff = 0;
-// 			for (var j = 0; j < userResponses.length; j++) {
-// 				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
-// 			}
-// 			// console.log('diff = ' + diff);
+		// Compute best friend match
+		var matchName = '';
+		var matchImage = '';
+		var totalDifference = 10000; // Make the initial value big for comparison
 
-// 			// If lowest difference, record the friend match
-// 			if (diff < totalDifference) {
-// 				// console.log('Closest match found = ' + diff);
-// 				// console.log('Friend name = ' + friends[i].name);
-// 				// console.log('Friend image = ' + friends[i].photo);
+		// Examine all existing friends in the list
+		for (var i = 0; i < friends.length; i++) {
+			console.log('friend = ' + JSON.stringify(friends[i]));
 
-// 				totalDifference = diff;
-// 				matchName = friends[i].name;
-// 				matchImage = friends[i].photo;
-// 			}
-// 		}
+			// Compute differenes for each question
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+			console.log('diff = ' + diff);
 
-// 		// Add new user
-// 		friends.push(userInput);
+			// If lowest difference, record the friend match
+			if (diff < totalDifference) {
+				console.log('Closest match found = ' + diff);
+				console.log('Friend name = ' + friends[i].name);
+				console.log('Friend image = ' + friends[i].photo);
 
-// 		// Send appropriate response
-// 		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
-// 	});
-// };
+				totalDifference = diff;
+				matchName = friends[i].name;
+				matchImage = friends[i].photo;
+			}
+		}
+
+		// Add new user
+		friends.push(userInput);
+
+		// Send appropriate response
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+	});
+};
--- server.js
+++ server.js
@@ -3,9 +3,16 @@ var bodyParser = require("body-parser");
 var path = require ("path");
 
 var app = express();
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3003;
 
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+
+// Start listening on PORT
+app.listen(PORT, function() {
+    console.log('Listening: ' + PORT);
+  });
